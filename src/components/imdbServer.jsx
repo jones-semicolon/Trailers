d1507f68862908a2f27264ab785bdea25e89ecdc
jones-semicolon <marjones.lapinig@gmail.com>
2024-10-11 17:17:42 +0800

    v0.3

--- src/components/imdbServer.jsx
+++ src/components/imdbServer.jsx
@@ -1,96 +1,123 @@
 import react from "react";
 import axios from "axios";
 
-const API_KEY = import.meta.env.VITE_TMDB_KEY;
-const BASED_URL = "https://api.themoviedb.org/2";
+const API_KEY = import.meta.env.VITE_TMDB_ACCESS_TOKEN;
+console.log(API_KEY);
+const BASED_URL = "https://api.themoviedb.org/3";
+const headers = {
+  accept: "application/json",
+  Authorization: `Bearer ${API_KEY}`,
+};
 
 export default {
   upcomingMovies() {
     return (async () => {
-      let res = await axios.get(
-        `${BASED_URL}/movie/upcoming?api_key=${API_KEY}&page=1`,
-      );
+      let res = await axios.request({
+        method: "GET",
+        url: `${BASED_URL}/movie/upcoming?api_key=${API_KEY}&page=1`,
+        headers,
+      });
       return await res.data;
     })();
   },
   topRatedMovies() {
     return (async () => {
-      let res = await axios.get(
-        `${BASED_URL}/movie/top_rated?api_key=${API_KEY}&language=en&page=1`,
-      );
+      let res = await axios.request({
+        method: "GET",
+        url: `${BASED_URL}/movie/top_rated?language=en&page=1`,
+        headers,
+      });
       return await res.data;
     })();
   },
   movieDetails(id) {
     return (async () => {
-      let res = await axios.get(
-        `${BASED_URL}/movie/${id}?api_key=${API_KEY}&language=en`,
-      );
+      let res = await axios.request({
+        method: "GET",
+        url: `${BASED_URL}/movie/${id}?language=en`,
+        headers,
+      });
       return await res.data;
     })();
   },
   movieCast(id) {
     return (async () => {
-      let res = await axios.get(
-        `${BASED_URL}/movie/${id}/credits?api_key=${API_KEY}&language=en`,
-      );
+      let res = await axios.request({
+        method: "GET",
+        url: `${BASED_URL}/movie/${id}/credits?language=en`,
+        headers,
+      });
       return await res.data;
     })();
   },
   popularMovies() {
     return (async () => {
-      let res = await axios.get(
-        `${BASED_URL}/movie/popular?api_key=${API_KEY}&language=en`,
-      );
+      let res = await axios.request({
+        method: "GET",
+        url: `${BASED_URL}/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`,
+        headers,
+      });
       return await res.data;
     })();
   },
   trendingMovies() {
     return (async () => {
-      let res = await axios.get(
-        `${BASED_URL}/trending/movie/week?api_key=${API_KEY}`,
-      );
+      let res = await axios.request({
+        method: "GET",
+        url: `${BASED_URL}/trending/movie/week`,
+        headers,
+      });
       return await res.data;
     })();
   },
   similarMovies(id) {
     return (async () => {
-      let res = await axios.get(
-        `${BASED_URL}/movie/${id}/similar?api_key=${API_KEY}&page=1`,
-      );
+      let res = await axios.request({
+        method: "GET",
+        url: `${BASED_URL}/movie/${id}/similar?page=1`,
+        headers,
+      });
       return await res.data;
     })();
   },
   recommendedMovies(id) {
     return (async () => {
-      let res = await axios.get(
-        `${BASED_URL}/movie/${id}/recommendations?api_key=${API_KEY}&page=1`,
-      );
+      let res = await axios.request({
+        method: "GET",
+        url: `${BASED_URL}/movie/${id}/recommendations?page=1`,
+        headers,
+      });
       return await res.data;
     })();
   },
   trailer(id) {
     return (async () => {
-      let res = await axios.get(
-        `${BASED_URL}/movie/${id}/videos?api_key=${API_KEY}`,
-      );
+      let res = await axios.request({
+        method: "GET",
+        url: `${BASED_URL}/movie/${id}/videos`,
+        headers,
+      });
       return await res.data;
     })();
   },
 
   backdrop(id) {
     return (async () => {
-      let res = await axios.get(
-        `${BASED_URL}/movie/${id}/images?api_key=${API_KEY}&language=en`,
-      );
+      let res = await axios.request({
+        method: "GET",
+        url: `${BASED_URL}/movie/${id}/images?language=en`,
+        headers,
+      });
       return await res.data;
     })();
   },
   search(query) {
     return (async () => {
-      let res = await axios.get(
-        `${BASED_URL}/search/movie?api_key=${API_KEY}&language=en&query=${query}`,
-      );
+      let res = await axios.request({
+        method: "GET",
+        url: `${BASED_URL}/search/movie?language=en&query=${query}`,
+        headers,
+      });
       return await res.data;
     })();
   },
